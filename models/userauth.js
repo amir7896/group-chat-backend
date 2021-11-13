adbbf91cde30b47f7edc85a6dac350a31e324c6f
amir7896 <amirshahzad07896@gmail.com>
2021-11-13 16:55:57 +0500

    First Commit

---
+++ models/userauth.js
@@ -0,0 +1,35 @@
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+var signupSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+
+    name:{
+        type:String,
+        required:true
+    },
+   nickName:{
+        
+        type:String,
+        required:true
+
+   },
+   mobNumber:{
+    type:Number,
+    required:true
+   },
+
+  email:{
+        type:String,
+        required:true
+    },
+  password:{
+        
+     type:String,
+     required:true
+
+    }
+
+});
+var Users = mongoose.model('Users',signupSchema);
+module.exports=Users;
